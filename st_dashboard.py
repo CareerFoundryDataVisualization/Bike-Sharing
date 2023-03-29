24e48e11608a6c01bb36b650710ca43595e75474
CareerFoundryDataVisualization <danadaskalova@gmail.com>
2023-03-29 14:43:44 +0300

    test

--- st_dashboard.py
+++ st_dashboard.py
@@ -1,7 +1,8 @@
+################################################ DIVVY BIKES DASHABOARD #####################################################
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
@@ -10,79 +11,70 @@ from keplergl import KeplerGl
 from datetime import datetime as dt
 
 
-
-
-#########################33 Initial settings for the dashboard ####################################################
+########################### Initial settings for the dashboard ##################################################################
 
 
 st.set_page_config(page_title = 'Divvy Bikes Strategy Dashboard', layout='wide')
-
 st.title("Divvy Bikes Strategy Dashboard")
 st.markdown("The dashboard will help with the expansion problems Divvy currently faces")
 st.markdown("Right now, Divvy bikes runs into a situation where customers complain about bikes not being avaibale at certain times. This analysis aims to look at the potential reasons behind this.")
-st.sidebar.title("Aspect Selector")
-st.sidebar.markdown("Select an aspect of the analysis:")
 
-page = st.sidebar.selectbox('Select page',
-  ["Most popular stations",
-    "Weather component and bike usage",
-    "Interactive map"])
+########################## Import data ###########################################################################################
+
+df = pd.read_csv('reduced_data_to_plot.csv', index_col = 0)
 
 
-df = pd.read_csv('df_to_plot_dashboard.csv', index_col = 0)
+# ######################################### DEFINE THE CHARTS #####################################################################
 
 
-######################################### DEFINE THE CHARTS #####################################################################
+## Groupby
+
+df['value'] = 1 
+df_groupby_bar = df.groupby('start_station_name', as_index=False).agg({'value': 'sum'})
+top20 = df_groupby_bar.nlargest(20, 'value')
+
+## Bar chart
+
+fig = go.Figure(go.Bar(x = top20['start_station_name'], y = top20['value'], marker={'color': top20['value'],'colorscale': 'Blues'}))
+fig.update_layout(
+    title = 'Top 20 most popular bike stations in Chicago',
+    xaxis_title = 'Start stations',
+    yaxis_title ='Sum of trips',
+    width = 900, height = 600
+)
+st.plotly_chart(fig, use_container_width=True)
 
 
-## Create the bar chart
+## Line chart 
 
-if page == 'Most popular stations':
+fig_2 = make_subplots(specs = [[{"secondary_y": True}]])
 
-    ## Bar chart
+fig_2.add_trace(
+go.Scatter(x = df['date'], y = df['bike_rides_daily'], name = 'Daily bike rides', marker={'color': df['bike_rides_daily'],'color': 'blue'}),
+secondary_y = False
+)
 
-    df['value'] = 1 
-    df_groupby_bar = df.groupby('start_station_name', as_index = False).agg({'value': 'sum'})
-    top20 = df_groupby_bar.nlargest(20, 'value')
-    fig = go.Figure(go.Bar(x = top20['start_station_name'], y = top20['value']))
+fig_2.add_trace(
+go.Scatter(x=df['date'], y = df['avgTemp'], name = 'Daily temperature', marker={'color': df['avgTemp'],'color': 'red'}),
+secondary_y=True
+)
 
-    fig.update_layout(
-        title = 'Most popular stations',
-        xaxis_title = 'Start stations',
-        yaxis_title='Sum of trips'
-    )
-    st.plotly_chart(fig, use_container_width=True)
+fig_2.update_layout(
+    title = 'Daily bike trips and temperatures in 2018',
+    height = 600
+)
 
-elif page == 'Weather component and bike usage':
-
-    ### Create the dual axis line chart ###
-
-    fig = make_subplots(specs = [[{"secondary_y": True}]])
-
-    fig.add_trace(
-    go.Scatter(x = df['date'], y = df['bike_rides_daily'], name = 'Daily bike rides'),
-    secondary_y = False
-    )
-
-    fig.add_trace(
-    go.Scatter(x=df['date'], y = df['avgTemp'], name = 'Daily temperature'),
-    secondary_y=True
-    )
-    st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig_2, use_container_width=True)
 
 
-else: 
+### Add the map ###
 
-    ### Create the map ###
+path_to_html = "Divvy Bike Trips Aggregated.html" 
 
-    st.write("Interactive map showing aggregated bike trips over Chicago")
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
 
-    path_to_html = "Divvy Bike Trips Aggregated.html" 
-
-    # Read file and keep in variable
-    with open(path_to_html,'r') as f: 
-        html_data = f.read()
-
-    ## Show in webpage
-    st.header("Aggregated Bike Trips in Chicago")
-    st.components.v1.html(html_data,height=1000)
+## Show in webpage
+st.header("Aggregated Bike Trips in Chicago")
+st.components.v1.html(html_data,height=1000)
